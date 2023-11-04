d375699a563c676ef1a5379202884d17fb35f0e1
eemiri <40573030+eemiri@users.noreply.github.com>
2023-11-04 15:47:53 +0100

    .

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -836,7 +836,7 @@
 			required: {
 				name: "Required",
 				type: "required",
-				message: "This is required<!-- data-validator-required-message to override -->"
+				message: "Benötigtes Feld<!-- data-validator-required-message to override -->"
 			},
 			checkone: {
 				name: "Checkone",
